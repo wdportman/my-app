254ae62d611c864629abc7ef0f38ff42a34e5ee9
Will <wdportman@gmail.com>
2021-02-09 13:20:48 -0800

    Create basic counter component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 import Button from './components/Button';
+import Counter from './components/Counter';
+import {useState} from 'react';
 
 function App() {
+
+  const [count, setCount] = useState(0);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <Button>Click here</Button>
-      </header>
-    </div>
+    <Counter/>
   );
 }
 
---
+++ src/components/Counter.js
@@ -0,0 +1,16 @@
+import Button from './Button';
+import React, { useState } from 'react';
+
+export default function Counter(props) {
+
+  const [count, setCount] = useState(0);
+
+  return (
+    <div>
+      <p>You clicked {count} times</p>
+      <button onClick={() => setCount(count + 1)}>
+        Click me
+    </button>
+    </div>
+  );
+}
