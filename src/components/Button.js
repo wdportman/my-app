c8589f6c47994027cb1339d2886b54af6e7e1b63
Will <wdportman@gmail.com>
2021-02-09 11:30:26 -0800

    Delete styling placeholders to start from scratch

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,18 +1,8 @@
 import React from 'react';
-import classNames from 'classnames';
-import "../styles/button.scss";
 
 export default function Button (props) {
-  // use classNames package to manage different types of button styles
-  const buttonClass = classNames(
-    'button',
-    {'button-confirm': props.confirm}, 
-    {'button-danger': props.danger},
-    {'button-disabled': props.disabled}
-  ); 
   return (
     <button
-      className={buttonClass}
       onClick={props.onClick}
       disabled={props.disabled}
     > 
