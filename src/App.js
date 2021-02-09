be447753afe8c18d53501cf58ec7b95882f07d5f
Will <wdportman@gmail.com>
2021-02-09 11:05:20 -0800

    Set up components & styles; add Node-Sass and Sass

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import Button from './stories/Button';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Button>Will</Button>
       </header>
     </div>
   );
