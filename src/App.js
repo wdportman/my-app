14be0a609d95c35770c350b72a30117a8de636e7
Will <wdportman@gmail.com>
2021-02-09 11:07:08 -0800

    Add basic button

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-import Button from './stories/Button';
+import Button from './components/Button';
 
 function App() {
   return (
---
+++ src/components/Button.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import classNames from 'classnames';
+// import "../styles/Button.scss";
+
+export default function Button (props) {
+  // use classNames package to manage different types of button styles
+  // const buttonClass = classNames(
+  //   'button',
+  //   {'button-confirm': props.confirm}, 
+  //   {'button-danger': props.danger},
+  //   {'button-disabled': props.disabled}
+  // ); 
+  return (
+    <button
+      // className={buttonClass}
+      onClick={props.onClick}
+      disabled={props.disabled}
+    > 
+    {props.children}
+    </button>
+  ); 
+}
