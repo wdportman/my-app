dd5c1e295a675877ca63ee2aa9993fb66c1576cc
Will <wdportman@gmail.com>
2021-02-24 18:06:12 -0800

    Work on RPS styling

--- src/components/pages/RPS.js
+++ src/components/pages/RPS.js
@@ -14,14 +14,22 @@ export default function RPS(props) {
   return (
     <div>
       <h3>Rock Paper Scissors</h3>
-      <p>Pick an option</p>
-      <div className='rps-images'>
-        <img className='rps-image' src={Rock} alt="Rock" onClick={() => handleClick("Rock")} />
-        <img className='rps-image' src={Paper} alt="Paper" onClick={() => handleClick("Paper")} />
-        <img className='rps-image' src={Scissors} alt="Scissors" onClick={() => handleClick("Scissors")} />
-      </div>
-      <div className='rps-computer'>
+      <div className='rps'>
+        <div className='rps-column'>
+          <p>Pick an option</p>
+          <div className='rps-images'>
+            <img className='rps-image' src={Rock} alt="Rock" onClick={() => handleClick("Rock")} />
+            <img className='rps-image' src={Paper} alt="Paper" onClick={() => handleClick("Paper")} />
+            <img className='rps-image' src={Scissors} alt="Scissors" onClick={() => handleClick("Scissors")} />
+          </div>
+        </div>
+        <div className='rps-column'>
+          <p>The computer chose...</p>
 
+        </div>
+        <div className='rps-column'>
+          <p>Scoreboard:</p>
+        </div>
       </div>
       <Link to="/">Home</Link>
     </div>
